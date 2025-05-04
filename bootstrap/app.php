d2c5e86e655587b66631e14c1b89847f813534a8
aymane boujeddi <aymane655@gmail.com>
2025-05-04 16:35:14 +0100

    fix(middleware) enhance profile completion check and ensure user role redirection so that the user can't access the completion pages if he already completed (using the URL)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckRole;
+use App\Http\Middleware\ProfileCompletion;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->alias(['checkrole' => CheckRole::class]);
+        $middleware->alias(['checkrole' => CheckRole::class,
+    'profileCompletion' => ProfileCompletion::class,]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
